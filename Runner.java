5b3e6449eba45a38bdaffc5def5abb808d956f0e
pekoraGaming21 <kevincheng655@gmail.com>
2025-05-20 22:44:35 -0700

    updated runner

--- Runner.java
+++ Runner.java
@@ -8,15 +8,18 @@ public class Runner{
         // calc.setAmp(1.0);
         // calc.calculate();
         Artifact[] list = {
-            new Artifact("HP",4780, new Substat("CD", 26.4), new Substat("CR", 7.4), new Substat("PATK", 0.105), new Substat("ATK",16)), 
-            new Artifact("ATK",311,new Substat("ER", 5.8),new Substat("CR", 7.0),new Substat("EM", 79), new Substat("CD", 13.2)),
-            new Artifact("EM", 187, new Substat("PDEF", 0.058), new Substat("CR", 8.6), new Substat("PHP", 0.041), new Substat("CD", 24.9)),
-            new Artifact("CDMG", 0.466, new Substat("HP", 209), new Substat("ATK", 29), new Substat("EM", 42), new Substat("CD", 27.2)),
-            new Artifact("CD", 62.2, new Substat("PATK", 0.105), new Substat("CR", 7.8), new Substat("ATK", 31), new Substat("EM", 42))
+            new Artifact("Flower", "HP",4780, new Substat("CD", 26.4), new Substat("CR", 7.4), new Substat("PATK", 0.105), new Substat("ATK",16)), 
+            new Artifact("Feather", "ATK",311,new Substat("ER", 5.8),new Substat("CR", 7.0),new Substat("EM", 79), new Substat("CD", 13.2)),
+            new Artifact("Sands", "EM", 187, new Substat("PDEF", 0.058), new Substat("CR", 8.6), new Substat("PHP", 0.041), new Substat("CD", 24.9)),
+            new Artifact("Goblet", "CDMG", 0.466, new Substat("HP", 209), new Substat("ATK", 29), new Substat("EM", 42), new Substat("CD", 27.2)),
+            new Artifact("Circlet", "CD", 62.2, new Substat("PATK", 0.105), new Substat("CR", 7.8), new Substat("ATK", 31), new Substat("EM", 42))
         };
 
-        Character ganyu = new Character(9796.73,334.85,630.21, list);
+        Character ganyu = new Character("Cryo", 90, 9796.73,334.85,630.21, "Hunter's Path", 541.83, list, "CD", 38.4);
         System.out.println(ganyu.getTotalATK());
+
+        BetterArtifacts test = new BetterArtifacts(ganyu);
+        System.out.println(test.randomize("Sands"));
     }
     
 }
